47fbc5bae72c0097c01aeae73210f81f2a5c4b2c
KristenNolting <knolt44@gmail.com>
2022-10-13 13:18:54 -0400

    added multiple regressions script and figure

--- Code/multiple_regressions.R
+++ Code/multiple_regressions.R
@@ -33,42 +33,10 @@ ks_mod <- brm(Ks_scaled ~ BW_scaled + WD_scaled + Leaf_Area_scaled +
               control = list(adapt_delta = 0.999, max_treedepth = 12),
               save_pars = save_pars(all = TRUE),
               iter = 4000, warmup = 2000, chains = 4,
-              file = "/Output/Model_Fits/ks_mod")
+              file = "Output/Model_Fits/ks_mod")
 
-# Model Checks
-summary(ks_mod, digits = 3, probs = c(0.025, 0.975))
-brms::bayes_R2(ks_mod)[1] # 0.36, Consistent from path model in Annals paper
-hist(brms::bayes_R2(ks_mod, summary = FALSE))
-hist(brms::bayes_R2(ks_mod, summary = FALSE, re_formula = NA))
-brms::bayes_R2(ks_mod, re_formula = NA)[1] # 0.20
-pp_check(ks_mod, type = "dens_overlay")
-pp_check(ks_mod, type = "intervals")
-pp_check(ks_mod, type = "loo_pit")
-ks_mod_loo <- loo(ks_mod, moment_match = TRUE)
-plot(ks_mod_loo, label_points = TRUE) # Points 16 and 38 with high Pareto-k values
-Protea_data[16,] # PRPU at JK
-Protea_data[38,] # PRRE at JK
-
-
-# Plot Observed versus Predicted
-p_post <- predict(ks_mod) # get predicted ks values from original data and fitted model
-p_post_Ks <- as.data.frame(p_post)
-p_post_Ks$Obs <- Protea_data$Ks_scaled
-p_post_Ks$Species <- Protea_data$Species
-p_post_Ks$Site <- Protea_data$Site
-ks_pp <- ggplot(data = p_post_Ks, aes(x = Estimate, y = Obs)) +
-  geom_point(size = 2, aes(shape = Site, colour = as.factor(Species))) +
-  #scale_color_viridis(discrete = TRUE) +
-  scale_fill_distiller(type = "seq", palette = "Spectral", direction = 1) +
-  geom_abline(slope = 1, colour = "grey44", linetype = "dashed") +
-  ggtitle("Sapwood-specific \n Hydraulic Conductance") + xlab("Predicted Ks (scaled)") + ylab("Observed Ks (scaled)") +
-  theme_classic() + labs(colour = "Species") +
-  theme(plot.title = element_text(size = 14, hjust = 0.5),
-        axis.title.x = element_text(size = 12),
-        axis.title.y = element_text(size = 12),
-        legend.position = "none")
-ks_pp
-
+bayes_R2(ks_mod)
+bayes_R2(ks_mod, re_formula = NA)
 
 
 # 2. Leaf Specific Conductivity
@@ -83,23 +51,106 @@ LSC_mod <- brm(LSC_scaled ~ BW_scaled + WD_scaled + Leaf_Area_scaled +
                control = list(adapt_delta = 0.999, max_treedepth = 12),
                save_pars = save_pars(all = TRUE),
                iter = 4000, warmup = 2000, chains = 4,
-               file = "/Users/knolting/Documents/UConn_Projects/MW2PAC/Model_Output/LSC_mod")
+               file = "Output/Model_Fits/LSC_mod")
 
-# Model Checks
-summary(LSC_mod, digits = 3, probs = c(0.025, 0.975))
-brms::bayes_R2(LSC_mod)[1] # 0.30, Consistent from path model in Annals paper
-hist(brms::bayes_R2(LSC_mod, summary = FALSE))
-hist(brms::bayes_R2(LSC_mod, summary = FALSE, re_formula = NA))
-brms::bayes_R2(LSC_mod, re_formula = NA)[1] # 0.22
-pp_check(LSC_mod, type = "dens_overlay")
-pp_check(LSC_mod, type = "intervals")
-pp_check(LSC_mod, type = "loo_pit_qq")
-LSC_mod_loo <- loo(LSC_mod, moment_match = TRUE) # not working - need to think about model fit
-LSC_mod_loo
-plot(LSC_mod_loo, label_points = TRUE)
+bayes_R2(LSC_mod)
+bayes_R2(LSC_mod, re_formula = NA)
 
 
-# Plot Observed versus Predicted
+# 3. Area-based photosynthetic rate
+Photo_mod <- brm(Photo_scaled ~ BW_scaled + WD_scaled + Leaf_Area_scaled + 
+                   LMA_scaled + LD_scaled + LWR_scaled + Stom_L_scaled + Stom_D_scaled +
+                   (1|Site) + (1|Species) + (1|Site:Species),
+                 data = Protea_data,
+                 family = gaussian(link = "identity"),
+                 prior = c(prior(normal(0, 1), class = Intercept),
+                           prior(normal(0, 1), class = b),
+                           prior(cauchy(0, 5), class = sd)),
+                 control = list(adapt_delta = 0.999, max_treedepth = 12),
+                 save_pars = save_pars(all = TRUE),
+                 iter = 4000, warmup = 2000, chains = 4,
+                 file = "Output/Model_Fits/Photo_mod")
+
+bayes_R2(photo_mod)
+bayes_R2(photo_mod, re_formula = NA)
+
+
+# 4. Total Assimilation (LSP)
+Total_Assim_mod <- brm(Total_Assim_scaled ~ BW_scaled + WD_scaled + Leaf_Area_scaled + 
+                         LMA_scaled + LD_scaled + LWR_scaled + Stom_L_scaled + Stom_D_scaled +
+                         (1|Site) + (1|Species) + (1|Site:Species),
+                       data = Protea_data,
+                       family = gaussian(link = "identity"),
+                       prior = c(prior(normal(0, 1), class = Intercept),
+                                 prior(normal(0, 1), class = b),
+                                 prior(cauchy(0, 5), class = sd)),
+                       control = list(adapt_delta = 0.999, max_treedepth = 12),
+                       save_pars = save_pars(all = TRUE),
+                       iter = 4000, warmup = 2000, chains = 4,
+                       file = "Output/Model_Fits/LSP_mod")
+
+bayes_R2(Total_Assim_mod)
+bayes_R2(Total_Assim_mod, re_formula = NA)
+
+
+# 5. Stomatal Conductance
+Cond_mod <- brm(Cond_scaled ~ BW_scaled + WD_scaled + Leaf_Area_scaled + 
+                  LMA_scaled + LD_scaled + LWR_scaled + Stom_L_scaled + Stom_D_scaled +
+                  (1|Site) + (1|Species) + (1|Site:Species),
+                data = Protea_data,
+                family = gaussian(link = "identity"),
+                prior = c(prior(normal(0, 1), class = Intercept),
+                          prior(normal(0, 1), class = b),
+                          prior(cauchy(0, 5), class = sd)),
+                control = list(adapt_delta = 0.999, max_treedepth = 12),
+                save_pars = save_pars(all = TRUE),
+                iter = 4000, warmup = 2000, chains = 4,
+                file = "Output/Model_Fits/Cond_mod")
+
+bayes_R2(Cond_mod)
+bayes_R2(Cond_mod, re_formula = NA)
+
+
+# 6. Instantaneous WUE
+WUE_Instan_mod <- brm(WUE_Instan_scaled ~ BW_scaled + WD_scaled + Leaf_Area_scaled + 
+                        LMA_scaled + LD_scaled + LWR_scaled + Stom_L_scaled + Stom_D_scaled +
+                        (1|Site) + (1|Species) + (1|Site:Species),
+                      data = Protea_data,
+                      family = gaussian(link = "identity"),
+                      prior = c(prior(normal(0, 1), class = Intercept),
+                                prior(normal(0, 1), class = b),
+                                prior(cauchy(0, 5), class = sd)),
+                      control = list(adapt_delta = 0.999, max_treedepth = 12),
+                      save_pars = save_pars(all = TRUE),
+                      iter = 4000, warmup = 2000, chains = 4,
+                      file = "Output/Model_Fits/WUE_mod")
+
+bayes_R2(WUE_Instan_mod)
+bayes_R2(WUE_Instan_mod, re_formula = NA)
+
+
+# Make a predicted versus observed plot for each model, and combine into multipanel plot
+
+# 1. Ks
+p_post <- predict(ks_mod) # get predicted ks values from original data and fitted model
+p_post_Ks <- as.data.frame(p_post)
+p_post_Ks$Obs <- Protea_data$Ks_scaled
+p_post_Ks$Species <- Protea_data$Species
+p_post_Ks$Site <- Protea_data$Site
+ks_pp <- ggplot(data = p_post_Ks, aes(x = Estimate, y = Obs)) +
+  geom_point(size = 2, aes(shape = Site, colour = as.factor(Species))) +
+  scale_fill_distiller(type = "seq", palette = "Spectral", direction = 1) +
+  geom_abline(slope = 1, colour = "grey44", linetype = "dashed") +
+  ggtitle("Sapwood-specific \n Hydraulic Conductance") + xlab("Predicted Ks (scaled)") + ylab("Observed Ks (scaled)") +
+  theme_classic() + labs(colour = "Species") +
+  theme(plot.title = element_text(size = 14, hjust = 0.5),
+        axis.title.x = element_text(size = 12),
+        axis.title.y = element_text(size = 12),
+        legend.position = "none")
+ks_pp
+
+
+# 2. LSC
 p_post <- predict(LSC_mod) # get predicted ks values from original data and fitted model
 p_post_LSC <- as.data.frame(p_post)
 p_post_LSC$Obs <- Protea_data$LSC_scaled
@@ -119,36 +170,7 @@ LSC_pp <- ggplot(data = p_post_LSC, aes(x = Estimate, y = Obs)) +
 LSC_pp
 
 
-# 3. Area-based photosynthetic rate
-Photo_mod <- brm(Photo_scaled ~ BW_scaled + WD_scaled + Leaf_Area_scaled + 
-                   LMA_scaled + LD_scaled + LWR_scaled + Stom_L_scaled + Stom_D_scaled +
-                   (1|Site) + (1|Species) + (1|Site:Species),
-                 data = Protea_data,
-                 family = gaussian(link = "identity"),
-                 prior = c(prior(normal(0, 1), class = Intercept),
-                           prior(normal(0, 1), class = b),
-                           prior(cauchy(0, 5), class = sd)),
-                 control = list(adapt_delta = 0.999, max_treedepth = 12),
-                 save_pars = save_pars(all = TRUE),
-                 iter = 4000, warmup = 2000, chains = 4,
-                 file = "/Users/knolting/Documents/UConn_Projects/MW2PAC/Model_Output/Photo_mod")
-
-# Model Checks
-summary(Photo_mod, digits = 3, probs = c(0.025, 0.975))
-brms::bayes_R2(Photo_mod)[1] # 0.44, Consistent from path model in Annals paper
-hist(brms::bayes_R2(Photo_mod, summary = FALSE)) 
-hist(brms::bayes_R2(Photo_mod, summary = FALSE, re_formula = NA))
-brms::bayes_R2(Photo_mod, re_formula = NA)[1] # 0.27
-pp_check(Photo_mod, type = "dens_overlay")
-pp_check(Photo_mod, type = "intervals")
-pp_check(Photo_mod, type = "loo_pit_qq")
-Photo_mod_loo <- loo(Photo_mod, moment_match = TRUE)
-Photo_mod_loo
-plot(Photo_mod_loo, label_points = TRUE) # Points 70 and 126 with high Pareto-k values
-Protea_data[c(70, 126),] # PRLA at CB and PRRE at SB
-
-
-# Plot Observed versus Predicted
+# 3. Photo
 p_post <- predict(Photo_mod) # get predicted ks values from original data and fitted model
 p_post_Photo <- as.data.frame(p_post)
 p_post_Photo$Obs <- Protea_data$Photo_scaled
@@ -168,35 +190,8 @@ Photo_pp <- ggplot(data = p_post_Photo, aes(x = Estimate, y = Obs)) +
         legend.position = "none")
 Photo_pp
 
-# 4. Total Assimilation (LSP)
-Total_Assim_mod <- brm(Total_Assim_scaled ~ BW_scaled + WD_scaled + Leaf_Area_scaled + 
-                         LMA_scaled + LD_scaled + LWR_scaled + Stom_L_scaled + Stom_D_scaled +
-                         (1|Site) + (1|Species) + (1|Site:Species),
-                       data = Protea_data,
-                       family = gaussian(link = "identity"),
-                       prior = c(prior(normal(0, 1), class = Intercept),
-                                 prior(normal(0, 1), class = b),
-                                 prior(cauchy(0, 5), class = sd)),
-                       control = list(adapt_delta = 0.999, max_treedepth = 12),
-                       save_pars = save_pars(all = TRUE),
-                       iter = 4000, warmup = 2000, chains = 4,
-                       file = "/Users/knolting/Documents/UConn_Projects/MW2PAC/Model_Output/LSP_mod")
 
-# Model Checks
-summary(Total_Assim_mod, digits = 3, probs = c(0.025, 0.975))
-brms::bayes_R2(Total_Assim_mod)[1] # 0.32, Consistent from path model in Annals paper
-hist(brms::bayes_R2(Total_Assim_mod, summary = FALSE))
-hist(brms::bayes_R2(Total_Assim_mod, summary = FALSE, re_formula = NA))
-brms::bayes_R2(Total_Assim_mod, re_formula = NA)[1] # 0.36
-pp_check(Total_Assim_mod, type = "dens_overlay")
-pp_check(Total_Assim_mod, type = "intervals")
-pp_check(Total_Assim_mod, type = "loo_pit_qq")
-Total_Assim_mod_loo <- loo(Total_Assim_mod, moment_match = TRUE)
-Total_Assim_mod_loo
-plot(Total_Assim_mod_loo, label_points = TRUE)
-
-
-# Plot Observed versus Predicted
+# 4. Total_Assim (LSP)
 p_post <- predict(Total_Assim_mod) # get predicted ks values from original data and fitted model
 p_post_Total_Assim <- as.data.frame(p_post)
 p_post_Total_Assim$Obs <- Protea_data$Total_Assim_scaled
@@ -216,32 +211,51 @@ Total_Assim_pp <- ggplot(data = p_post_Total_Assim, aes(x = Estimate, y = Obs)) 
 Total_Assim_pp
 
 
-# 5. Stomatal Conductance
-Cond_mod <- brm(Cond_scaled ~ BW_scaled + WD_scaled + Leaf_Area_scaled + 
-                  LMA_scaled + LD_scaled + LWR_scaled + Stom_L_scaled + Stom_D_scaled +
-                  (1|Site) + (1|Species) + (1|Site:Species),
-                data = Protea_data,
-                family = gaussian(link = "identity"),
-                prior = c(prior(normal(0, 1), class = Intercept),
-                          prior(normal(0, 1), class = b),
-                          prior(cauchy(0, 5), class = sd)),
-                control = list(adapt_delta = 0.999, max_treedepth = 12),
-                save_pars = save_pars(all = TRUE),
-                iter = 4000, warmup = 2000, chains = 4,
-                file = "/Users/knolting/Documents/UConn_Projects/MW2PAC/Model_Output/Cond_mod")
-
-# Model Checks
-summary(Cond_mod, digits = 3, probs = c(0.025, 0.975))
-brms::bayes_R2(Cond_mod)[1] # 0.37
-hist(brms::bayes_R2(Cond_mod, summary = FALSE))
-hist(brms::bayes_R2(Cond_mod, summary = FALSE, re_formula = NA))
-brms::bayes_R2(Cond_mod, re_formula = NA)[1] # 0.18
-pp_check(Cond_mod, type = "dens_overlay")
-pp_check(Cond_mod, type = "intervals")
-pp_check(Cond_mod, type = "loo_pit_qq")
-Cond_mod_loo <- loo(Cond_mod, moment_match = TRUE)
-Cond_mod_loo
-plot(Cond_mod_loo, label_points = TRUE) # Points 42 and 51 with high Pareto-k values
-Protea_data[c(42, 51),] # BOTH PRLA at JK
+# 5. Stomtal Conductance
+p_post <- predict(Cond_mod) # get predicted ks values from original data and fitted model
+p_post_Cond <- as.data.frame(p_post)
+p_post_Cond$Obs <- Protea_data$Cond_scaled
+p_post_Cond$Species <- Protea_data$Species
+p_post_Cond$Site <- Protea_data$Site
+Cond_pp <- ggplot(data = p_post_Cond, aes(x = Estimate, y = Obs)) +
+  geom_point(size = 2, aes(shape = Site, colour = as.factor(Species))) +
+  scale_fill_distiller(type = "seq", palette = "Spectral", direction = 1) +
+  #scale_color_viridis(discrete = TRUE) +
+  geom_abline(slope = 1, colour = "grey44", linetype = "dashed") +
+  ggtitle("Stomatal \n Conductance") + xlab("Predicted gs (scaled)") + ylab("Observed gs (scaled)") +
+  theme_classic() + labs(colour = "Species") +
+  theme(plot.title = element_text(size = 14, hjust = 0.5),
+        axis.title.x = element_text(size = 12),
+        axis.title.y = element_text(size = 12),
+        legend.position = "none")
+Cond_pp
 
 
+# 6. WUE_Instan
+p_post <- predict(WUE_Instan_mod) # get predicted ks values from original data and fitted model
+p_post_WUE_Instan <- as.data.frame(p_post)
+p_post_WUE_Instan$Obs <- Protea_data$WUE_Instan_scaled
+p_post_WUE_Instan$Species <- Protea_data$Species
+p_post_WUE_Instan$Site <- Protea_data$Site
+WUE_Instan_pp <- ggplot(data = p_post_WUE_Instan, aes(x = Estimate, y = Obs)) +
+  geom_point(size = 2, aes(shape = Site, colour = as.factor(Species))) + 
+  scale_fill_distiller(type = "seq", palette = "Spectral", direction = 1) +
+  #scale_color_viridis(discrete = TRUE) +
+  geom_abline(slope = 1, colour = "grey44", linetype = "dashed") +
+  ggtitle("Instantaneous \n Water-use Efficiency") + xlab("Predicted WUE_Instan (scaled)") + ylab("Observed WUE_Instan (scaled)") +
+  theme_classic() + labs(colour = "Species") +
+  theme(plot.title = element_text(size = 14, hjust = 0.5),
+        axis.title.x = element_text(size = 12),
+        axis.title.y = element_text(size = 12),
+        legend.position = "none")
+WUE_Instan_pp
+
+
+# Panel Plot
+plot_grid(ks_pp, LSC_pp, Photo_pp, Total_Assim_pp, Cond_pp, WUE_Instan_pp,
+          labels = c("A", "B", "C", "D", "E", "F"), ncol = 3, nrow = 2)
+
+# save
+ggsave(filename = "trait-performance-regression_panel-plot.pdf", 
+       path = "Output/Figures",
+       height = 6, width = 10)
